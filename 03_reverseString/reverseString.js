860a51cf2a3d65cbb3e0494949002a4dcd37b547
Alexander Müller <nckgr1234@gmail.com>
2023-10-10 23:14:55 +0200

    Solve 03 reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(givenString) {
+    let splitString= givenString.split('');
+    let loopLength=splitString.length
+    let outPutStringArray=[];
+    //go through array and put last element at current position in array
+    for (let index = 0; index < loopLength; index++) {
+        outPutStringArray[index] =splitString.pop()
+    }
+    return outPutStringArray.join('')
 };
 
 // Do not edit below this line
