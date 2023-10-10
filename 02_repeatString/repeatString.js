34cd73578c50f72f95ea28228fa7d6e20feb0381
Alexander Müller <nckgr1234@gmail.com>
2023-10-10 22:58:11 +0200

    Solve 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(givenString, timesToRepeat) {
+    let resultingString="";
+    if(timesToRepeat<0){
+        return 'ERROR';
+    }
+    for (let index = 0; index < timesToRepeat; index++) {
+        resultingString=resultingString+givenString
+    }
+    return resultingString
 };
 
 // Do not edit below this line
