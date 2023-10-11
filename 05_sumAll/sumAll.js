85841ec9e0a3bfee8d48a6adf75a7d1dc8a5912d
Alexander Müller <nckgr1234@gmail.com>
2023-10-11 23:06:30 +0200

    Solve 05 sumall

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(firstInt,secondInt) {
+    if(typeof(firstInt)!="number"
+    ||typeof(secondInt)!="number"
+    ||firstInt<0
+    ||secondInt<0){
+        return "ERROR"
+    }
 
+    if (firstInt>secondInt) {
+        let swapVar=firstInt;
+        firstInt=secondInt;
+        secondInt=swapVar;
+    }
+    let summedInts=0
+    for (let i = firstInt; i <= secondInt; i++) {
+        summedInts+=i
+    }
+    return summedInts
 };
 
 // Do not edit below this line
