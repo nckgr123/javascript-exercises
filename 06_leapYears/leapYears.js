8f651db5be1bdfeaa791f71c4aa6a9eefdc1b35d
Alexander Müller <nckgr1234@gmail.com>
2023-10-11 23:15:47 +0200

    Solve 06 leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(givenYear) {
+    if (givenYear%4===0) {
+        if(givenYear%400===0){
+            return true;
+        }
+        else if(givenYear%100===0){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    return false
 };
 
 // Do not edit below this line
