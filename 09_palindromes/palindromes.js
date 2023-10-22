7f8372273c8d92ad135fb25b15ffe48782f6eea2
Alexander Müller <nckgr1234@gmail.com>
2023-10-22 15:38:52 +0200

    Add palindromes implementation

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (stringInput) {
+    let inputArr=processArray(stringInput.split(""));
+    let arrReverse=processArray(stringInput.split("").reverse());
+    return(arrReverse.join('')===inputArr.join(''))
 };
+function processArray(arr){
+    const punctuationChars=[",",".","!"," "]
+    // RM punctuation
+    arr= arr.filter(element=>!punctuationChars.includes(element))
+    //map to lowercase
+    arr= arr.map(element=>element.toLowerCase())
+    return arr;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
