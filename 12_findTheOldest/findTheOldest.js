a132a99bcb452510ed2cb4ed2ee013123e390600
Alexander Müller <nckgr1234@gmail.com>
2023-10-22 21:11:05 +0200

    Add FindTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((person,oldestKnown)=>{
+        if(getAge(person)>getAge(oldestKnown)) return person;
+        else return oldestKnown;
+    })
 };
+function getAge(person){
+    if(person.yearOfDeath){
+        return person.yearOfDeath-person.yearOfBirth;
+    }
+    else return new Date().getFullYear -person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
