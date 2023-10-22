f4957409a0566a99a8b2152dd4852c3566039388
Alexander Müller <nckgr1234@gmail.com>
2023-10-22 21:00:43 +0200

    Add fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(numGiven) {
+    if(numGiven<0)return "OOPS";
+    if(numGiven===0)return 1;
+    numGiven= +numGiven;
+    let sequence = [0,1]
+    //gen sequence for numGiven
+    for(let i=1;i<numGiven;i++){
+        sequence.push(sequence[sequence.length-1]+sequence[sequence.length-2])
+    }
+    return sequence.pop()
 
 };
 
